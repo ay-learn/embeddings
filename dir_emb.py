c3929ed1ebc482e7cca9c918aaa33790aa1283ec
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-05-18 19:22:28 +0100

    init

---
+++ dir_emb.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import argparse
+import os
+
+from add_emb import add_to_database
+
+def list_files(dir):
+    """Returns a list of all files in the given directory and its subdirectories"""
+    files = []
+    for root, _ , filenames in os.walk(dir):
+        for filename in filenames:
+            filepath = os.path.join(root, filename)
+            if os.path.getsize(filepath) > 0: # check if file is not empty
+                files.append(filepath)
+    return files
+
+def main():
+    parser = argparse.ArgumentParser(description="List all files in a directory")
+    parser.add_argument("dir", type=str, help="Path to directory")
+    args = parser.parse_args()
+
+    # Check if the directory exists
+    if not os.path.isdir(args.dir):
+        print(f"Directory '{args.dir}' not found.")
+        return
+
+    # List and print all files in the directory
+    files = list_files(args.dir)
+    print(f"There are {len(files)} files in '{args.dir}':")
+    for file in files:
+        print(f"file: {file}")
+        add_to_database(file)
+
+
+if __name__ == "__main__":
+    main()
