584544a51a9d55c7641be762390f2356c3b23dc9
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-05-19 04:22:50 +0100

    (add_emb.py) support md/py/latex formal

--- add_emb.py
+++ add_emb.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
 import argparse
 import os
+import pathlib
 
-from langchain.document_loaders import TextLoader
+# from langchain.document_loaders import TextLoader
 from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import MarkdownTextSplitter
+from langchain.text_splitter import LatexTextSplitter
+from langchain.text_splitter import PythonCodeTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 
 # from langchain.embeddings.openai import OpenAIEmbeddings
 
 
-def create_database(documents):
+
+def create_database(docs):
     persist_directory = "/data/projects/embedding/hf"
     parent_directory = os.path.dirname(persist_directory)
 
     if not os.path.exists(parent_directory):
         print("Parent directory does not exist!")
         return
-
-    # TODO, return text by our type: markdown, text, pdf ...
-    # text_splitter = get_from_type() : implimet this function
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    docs = text_splitter.split_documents(documents)
 
     # embeddings = OpenAIEmbeddings()
     embeddings = HuggingFaceEmbeddings()
@@ -31,14 +31,33 @@ def create_database(documents):
     )
     vectordb.persist()
 
+
 def add_to_database(file):
-    loader = TextLoader(file)
-    documents = loader.load()
+    file = pathlib.Path(file).read_text()
+    if file.endswith(".py"):
+        python_splitter = PythonCodeTextSplitter(chunk_size=30, chunk_overlap=0)
+        docs = python_splitter.create_documents([file])
+    if file.endswith(".tex"):
+        latex_splitter = LatexTextSplitter(chunk_size=400, chunk_overlap=0)
+        docs = latex_splitter.create_documents([file])
+    if file.endswith(".md"):
+        markdown_splitter = MarkdownTextSplitter(chunk_size=1000, chunk_overlap=50)
+        docs = markdown_splitter.create_documents([file])
+    else:
+        text_splitter = RecursiveCharacterTextSplitter(
+            # Set a really small chunk size, just to show.
+            chunk_size = 200,
+            chunk_overlap  = 20,
+            length_function = len,
+        )
+
+        docs = text_splitter.create_documents([file])
     try:
-        create_database(documents)
+        create_database(docs)
     except Exception as e:
         print(f"An error occurred while persisting the vectordb: {e}")
     print("---")
+
 
 def main():
     parser = argparse.ArgumentParser(description="Embedding this file")
